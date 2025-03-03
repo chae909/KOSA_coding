a72b2f829cbab653082553e7c82274715967e571
chae909 <jeongyoon2004@naver.com>
2025-03-03 13:17:23 +0900

    Contact_ver2

--- ContactMain.java
+++ ContactMain.java
@@ -8,18 +8,19 @@ public class ContactMain {
         int count = 0;
 
         while (true){
-            System.out.println("1. 추가\t2. 전체출력\t3. 검색\t4. 종료");
+            System.out.println("1. 추가 2. 전체출력 3. 검색 4. 종료");
+            System.out.print("메뉴번호를 선택하세요: ");
             String menu = sc.nextLine();
             switch (menu) {
                 case "1":
-                    System.out.println("이름: ");
+                    System.out.print("이름: ");
                     String name = sc.nextLine();
-                    System.out.println("전화번호: ");
+                    System.out.print("전화번호: ");
                     String number = sc.nextLine();
-                    System.out.println("생년월일: ");
+                    System.out.print("생년월일: ");
                     String birth = sc.nextLine();
                     arr[count++] = new Contact(name, number, birth);
-                    System.out.println("전화번호가 등록 되었습니다.");
+                    System.out.print("전화번호가 등록 되었습니다.");
                     break;
                 case "2":
                     for (int i = 0; i < count; i++) {
@@ -27,7 +28,7 @@ public class ContactMain {
                     }
                     break;
                 case "3":
-                    System.out.println("이름 검색색: ");
+                    System.out.print("이름 검색: ");
                     String searchName = sc.nextLine();
                     for (int i = 0; i < count; i++) {
                         arr[i].searchContact(searchName);
