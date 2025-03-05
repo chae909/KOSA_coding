d1bdeb1d7b7e035726c27e7b462d74c8733ebece
chae909 <jeongyoon2004@naver.com>
2025-03-05 17:16:02 +0900

    main수정정

--- AI_Agent/src/kosa/video/Main.java
+++ AI_Agent/src/kosa/video/Main.java
@@ -10,6 +10,9 @@ public class Main {
 		GeneralMember gm1 = new GeneralMember("aaa", "채정윤", "서울시");
 		GeneralMember gm2 = new GeneralMember("bbb", "채종민", "과천시");
 		GeneralMember gm3 = new GeneralMember("ccc", "채혜원", "안양시");
+		
+		SpecialMember sm1 = new SpecialMember("ddd", "신숙영", "안양시", 10);
+		SpecialMember sm2 = new SpecialMember("eee", "채현석", "안양시", 50);
 
 		gm1.rent(v1);
 		
@@ -17,11 +20,18 @@ public class Main {
 		
 		gm3.rent(v3);
 		
+		sm1.rent(v2);
+		sm2.rent(v1);
+		
 		gm1.printMember();
 		System.out.println("---------------------------------");
 		gm2.printMember();
 		System.out.println("---------------------------------");
 		gm3.printMember();
+		System.out.println("---------------------------------");
+		sm1.printSMember();
+		System.out.println("---------------------------------");
+		sm2.printSMember();
 		
 		
 	}
